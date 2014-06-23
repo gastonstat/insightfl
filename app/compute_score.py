d0ea19447c2f8ac82b397fb1ddfeae6328e50186
Gaston Sanchez <gaston.stat@gmail.com>
2014-06-22 20:11:03 -0700

    improve score

--- app/compute_score.py
+++ app/compute_score.py
@@ -2,14 +2,20 @@ import pymysql
 import sys
 
 
-cluster_avoid = ['after-shave','anti-aging','baby-wipes','concealer-foundation','eye-makeup',
-'feminine-moisturizer','foot-care','fragrance-for-men','fragrance-for-women',
+cluster_avoid = ['after-shave','anti-aging','baby-sunscreen','baby-wipes','concealer-foundation','eye-makeup',
+'foot-care','fragrance-for-men','fragrance-for-women',
 'hand-sanitizer','moisturizer','sunless-tanning','sunscreen-below-spf-15',
 'sunscreen-spf-15-above','tanning-oil']
 
-cluster_average = ['after-shave','baby-wipes','deodorants-antiperspirants-mens',
-'feminine-moisturizer','fragrance-for-men','fragrance-for-women','feminine-moisturizer',
-'fragrance-for-men','fragrance-for-women','hair-spray','mouthwash','shaving-cream',
+cluster_average = ['after-shave','baby-wipes','bubble-bath','concealer-foundation','conditioner',
+'deodorants-antiperspirants-mens','deodorants-antiperspirants-womens','eye-makeup',
+'feminine-moisturizer','feminine-powder_deodorant','foot-care','fragrance-for-men',
+'fragrance-for-women','hair-spray','moisturizer','mouthwash','personal-cleansing',
+'scrubs-oils-powders','shaving-cream','sunless-tanning','sunscreen-below-spf-15','tanning-oil','toothpaste']
+
+cluster_ok = ['after-shave','baby-wipes','conditioner','deodorants-antiperspirants-mens',
+'deodorants-antiperspirants-womens','feminine-moisturizer','foot-care','fragrance-for-men',
+'fragrance-for-women','hair-spray','mouthwash','personal-cleansing','shaving-cream',
 'sunless-tanning','tanning-oil']
 
 
@@ -28,22 +34,34 @@ def compute_score(category, num_ings, has_toxic):
     if has_toxic == 'yes':
         final_score = '0.0'
     else:
-        if num_ings >= 16:
+        if num_ings >= 15:
             if category in cluster_avoid:
-                final_score = 2
+                if num_ings >= 21:
+                    final_score = 2
+                else:
+                    final_score = 3
             else:
-                final_score = 4
+                if num_ings >= 21:
+                    final_score = 4
+                else:
+                    final_score = 5
         else:
             if num_ings >= 10:
-                final_score = 5
+                if category in cluster_average:
+                    final_score = 5
+                else:
+                    final_score = 7
             else:
                 if num_ings == 0:
                     final_score = tmp_score
                 else:
-                    if category in cluster_average:
-                        final_score = 5
+                    if category in cluster_ok:
+                        final_score = 6
                     else:
-                        final_score = 7
+                        if num_ings > 5:
+                            final_score = 7
+                        else:
+                            final_score = 8
     return final_score
     
  
